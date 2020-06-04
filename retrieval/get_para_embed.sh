0ede3d56feaaac702635c04f26e5925361a5b5cb
Wenhan Xiong <xwhan@cs.ucsb.edu>
2020-06-03 20:31:25 -0700

    Update get_para_embed.sh

--- retrieval/get_para_embed.sh
+++ retrieval/get_para_embed.sh
@@ -4,8 +4,8 @@
     --prefix eval-para \
     --predict_batch_size 300 \
     --bert_model_name bert-base-uncased \
-    --efficient_eval \
-    --predict_file ../data/para_doc.db \
+    --fp16 \
+    --predict_file ../data/wiki_splits.txt \
     --init_checkpoint logs/retrieve_train.txt-seed87-bsz640-fp16True-retriever_pretraining_single-lr1e-05-bert-base-uncased-filterTrue/checkpoint_best.pt \
     --embed_save_path encodings/para_embed.npy \
     --eval-workers 32 \
